02bbb86d2f8d1d8f4adcd57c6438c92f90e03ad6
Vaishali <grvaishali@gmail.com>
2020-10-07 20:10:39 -0400

    Form Test

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,15 @@ export default function Form(props) {
 
     const [name, setName] = useState(props.name || "");
     const [interviewer, setInterviewer] = useState(props.interviewer || null);
+    const [error, setError] = useState("");
+    function validate() {
+      if (name === "") {
+        setError("Student name cannot be blank");
+        return;
+      }
+      setError("");
+      props.onSave(name, interviewer);
+    }
 
     const reset = () => {
         setName("");
@@ -34,7 +43,9 @@ export default function Form(props) {
                         placeholder="Enter Student Name"
                         value={name}
                         onChange={e => setName(e.target.value)}
+                        data-testid="student-name-input"
                     />
+                    <section className="appointment__validation">{error}</section>
                 </form>
                 <InterviewerList
                     interviewers={props.interviewers}
@@ -44,7 +55,7 @@ export default function Form(props) {
             <section className="appointment__card-right">
                 <section className="appointment__actions">
                     <Button onClick={cancel} danger>Cancel</Button>
-                    <Button onClick={save} confirm>Save</Button>
+                    <Button onClick={validate} confirm>Save</Button>
                 </section>
             </section>
         </main>
