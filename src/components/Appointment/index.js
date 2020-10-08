078d5a0797739e0dacabae42f4ffba7e6e5e998c
Vaishali <grvaishali@gmail.com>
2020-10-07 22:25:38 -0400

    JEST tests with DOM - Having issues

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -58,7 +58,7 @@ export default function Appointment(props) {
             .catch(error => transition(ERROR_DELETE, true));
     }
 
-    return (<article className="appointment">
+    return (<article className="appointment" data-testid="appointment">
         <Header time={props.time} />
         {mode === EMPTY && <Empty onAdd={add} />}
 
