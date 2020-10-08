0ef9270bb7af3ed2a9b2cb59e2f7d4d30536c57c
Vaishali <grvaishali@gmail.com>
2020-10-07 20:03:13 -0400

    Application Test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,24 @@
+/*
+  We are rendering `<Application />` down below, so we need React.createElement
+*/
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+/*
+  We import our helper functions from the react-testing-library
+  The render function allows us to render Components
+*/
+import { render } from "@testing-library/react";
 
-import Application from "components/Application";
+/*
+  We import the component that we are testing
+*/
+import Appointment from "components/Appointments/index";
 
-afterEach(cleanup);
-
-it("renders without crashing", () => {
-  render(<Application />);
+/*
+  A test that renders a React Component
+*/
+describe("Appointment", () => {
+  it("renders without crashing", () => {
+    render(<Appointment />);
+  });
 });
